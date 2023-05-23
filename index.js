a05a1cc3ddc6b12e3168aa8120bbc03c35fe81b6
Nestor Hugo <nestorklauck@gmail.com>
2023-05-23 17:07:26 -0300

    code format

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-import Viagem from "./viagem-class.js";
-import ViagemInternacional from "./viagem-class.js";
+import Viagem from './viagem-class.js';
+import ViagemInternacional from './viagem-class.js';
 
-let viagemFozDoIguacu = new Viagem("Foz do Iguaçu", "11/10/2023", 3, 909.99); //criando
+// eslint-disable-next-line max-len
+let viagemFozDoIguacu = new Viagem('Foz do Iguaçu', '11/10/2023', 3, 909.99); //criando
 viagemFozDoIguacu.exibir(); //exibindo info
 
-let viagemFortaleza = new Viagem("Fortaleza", "16/11/2023", 4, 1439.99); //criando
+// eslint-disable-next-line max-len
+let viagemFortaleza = new Viagem('Fortaleza', '16/11/2023', 4, 1439.99); //criando
 viagemFortaleza.exibir(); //exibindo info
 
-let viagemRioDeJaneiro = new Viagem("Rio de Janeiro", "25/11/2023", 3, 1299.99); //criando
+// eslint-disable-next-line max-len
+let viagemRioDeJaneiro = new Viagem('Rio de Janeiro', '25/11/2023', 3, 1299.99); //criando
 viagemRioDeJaneiro.exibir(); //exibindo info
 
-let viagemFernandoDeNoronha = new Viagem("Fernando de Noronha", "10/12/2023", 3, 5245); //criando
+let viagemFernandoDeNoronha = new Viagem(
+  'Fernando de Noronha',
+  '10/12/2023',
+  3,
+  5245
+); //criando
 viagemFernandoDeNoronha.exibir(); //exibindo info
 
-let viagemIntBuenosAires = new ViagemInternacional("Buenos Aires", "12/07/2023", 4, 159329, "ARS"); //criando
+// eslint-disable-next-line max-len
+let viagemIntBuenosAires = new ViagemInternacional(
+  'Buenos Aires',
+  '12/07/2023',
+  4,
+  159329,
+  'ARS'
+); //criando
 viagemIntBuenosAires.exibir(); //exibindo info internacional
 
-var imagens = document.querySelectorAll(".card img");
+var imagens = document.querySelectorAll('.card img');
 
 for (var i = 0; i < imagens.length; i++) {
-  imagens[i].onmousemove = function() {
-    this.style.border = "5px solid #0D6EFD";
+  imagens[i].onmousemove = function () {
+    this.style.border = '5px solid #0D6EFD';
   };
-  
-  imagens[i].onmouseout = function() {
-    this.style.border = "none";
+
+  imagens[i].onmouseout = function () {
+    this.style.border = 'none';
   };
 }
