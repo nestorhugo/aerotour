9ef49128c64c0613a1a6da0f8779d2d72bd656f0
Nestor Hugo <nestorklauck@gmail.com>
2023-05-23 14:27:59 -0300

    ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        semi: ["error", "always"],
+        quotes: ["error", "single"],
+        indent: ["error", 4],
+        "comma-spacing": ["error", {before: false, after:true}],
+    }
+}
