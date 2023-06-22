5bbc71b1ae56cdb2efa52eb097d4d237e97037e6
Nestor Hugo <nestorklauck@gmail.com>
2023-06-21 23:58:53 -0300

    ajax json

--- app/pages/passagens/passagens.js
+++ app/pages/passagens/passagens.js
@@ -20,3 +20,29 @@ setInterval(atualizarContagemRegressiva, 1000);
 window.onload = function () {
   alert('Aproveite os preços! Sempre temos promoção!');
 };
+
+//Requisições AJAX
+$(document).ready(function () {
+  $('#btnDestinos').click(function () {
+    $.ajax({
+      url: '/destinos',
+      type: 'GET',
+      success: function (destinos) {
+        console.log(destinos);
+      },
+    });
+  });
+});
+
+$(document).ready(function () {
+  $('#btnVoos').click(function () {
+    $.ajax({
+      url: 'http://localhost:3000/voos',
+      type: 'GET',
+      success: function (voos) {
+        // Aqui você pode manipular os dados retornados da rota /voos
+        console.log(voos);
+      },
+    });
+  });
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado na porta ${port}`);
+});
+
+app.get('/destinos', (req, res) => {
+  const destinos = [
+    'Foz do Iguaçu',
+    'Fortaleza',
+    'Rio de Janeiro',
+    'Fernando de Noronha',
+    'Buenos Aires',
+    'Recife',
+    'Porto Seguro',
+    'Asunción',
+  ];
+  res.json(destinos);
+});
+
+app.get('/destinos', (req, res) => {
+  console.log('Requisição GET recebida em /destinos');
+});
